6ecb189fea71e8455dc492fa6c5cf3199be532a2
Miguel Angel Martinez Simon <mms172@inlumine.ual.es>
2025-06-07 19:50:49 +0200

    Correos

--- spring-skeleton/src/main/java/interfaz/Eliminarpropiacuenta.java
+++ spring-skeleton/src/main/java/interfaz/Eliminarpropiacuenta.java
@@ -125,6 +125,9 @@ public class Eliminarpropiacuenta extends VistaEliminarpropiacuenta  {
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.ssl.protocols", "TLSv1.2");  // Importante si usas Java 8
+		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");  // Confiar en Gmail explícitamente
+	//	props.put("mail.smtp.ssl.checkserveridentity", "false");  // Este en principio no se necesita
 
 		Session sesion = Session.getInstance(props, new Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
--- spring-skeleton/src/main/java/interfaz/ListaUsuariosGeneral_item.java
+++ spring-skeleton/src/main/java/interfaz/ListaUsuariosGeneral_item.java
@@ -14,10 +14,10 @@ public class ListaUsuariosGeneral_item extends VistaListausuariosgeneral_item{
 		this.u=u;
 		_listaUsuariosGeneral  = lista; 
 		this.getLabelNick().setText(u.getNick());
-		this.getLabelNumSeguidores().setText(String.valueOf(u.seguidoPor.size()) + (u.seguidoPor.size()>1?"Seguidores":" Seguidor" ));
-		this.getLabelNumSeguidos().setText(String.valueOf(u.sigueA.size()) + (u.sigueA.size()>1?"Seguidos":" Seguido" ) );
+		this.getLabelNumSeguidores().setText(String.valueOf(u.seguidoPor.size()) + (u.seguidoPor.size()>1?" Seguidores":" Seguidor" ));
+		this.getLabelNumSeguidos().setText(String.valueOf(u.sigueA.size()) + (u.sigueA.size()>1?" Seguidos":" Seguido" ) );
 		this.getImgPerfilUsuario().setSrc(u.getFotoPerfil());
-		this.getLabelNumTweets().setText(String.valueOf(u.escribe.size()) +(u.escribe.size()>1?"Escritos":" Escrito" )  );
+		this.getLabelNumTweets().setText(String.valueOf(u.escribe.size()) +(u.escribe.size()>1?" Escritos":" Escrito" )  );
 		
 		this.getImgPerfilUsuario().setHeight("50px");
 		this.getImgPerfilUsuario().setWidth("10px");
