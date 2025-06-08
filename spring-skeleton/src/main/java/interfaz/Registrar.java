7dcd025c0e2fbc521f9df6ba5fc4adbcb01d75bc
Miguel Angel Martinez Simon <mms172@inlumine.ual.es>
2025-06-08 12:06:02 +0200

    Condiciones de la contraseña

--- spring-skeleton/src/main/java/interfaz/Registrar.java
+++ spring-skeleton/src/main/java/interfaz/Registrar.java
@@ -77,6 +77,22 @@ public class Registrar extends VistaRegistrar {
 			Notification.show("Ya existe un usuario con ese nick.");
 			return;
 		}
+		Boolean notificationPassword = false;
+		if(!contrasena.matches(".*[!\"#$%&'()*+,\\-./:;<=>?@\\[\\\\\\]^_`{|}~].*")) {
+			Notification.show("La contraseña debe tener un caracter especial.");
+			notificationPassword=true;
+		}
+		if(!contrasena.matches(".*[A-Z].*") || !contrasena.matches(".*[a-z].*")) {
+			Notification.show("La contraseña debe tener al menos una mayuscula y una minuscula.");
+			notificationPassword=true;
+		}
+		if(contrasena.length()<8 || contrasena.length()>14) {
+			Notification.show("La contraseña debe ser entre 8 y 14 caracteres");
+			notificationPassword=true;
+		}
+		if(notificationPassword) {
+			return;
+		}
 		
 		// Comprobamos que la foto(perfil) lo sea realmente
 		if (!fotoPerfil.isBlank()) {
